56538c5bf5367740aa19f01707b6c82b4d4311c8
Juliia Prokopenko <atlasovaa13@gmail.com>
2024-08-11 10:55:12 +0200

    install libraries

---
+++ src/js/1-timer.js
@@ -0,0 +1,10 @@
+//Бібліотека flatpickr
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+//Бібліотека повідомлень
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
